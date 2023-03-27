6c53a8f49d9b991fde20af9ac86b5d31c4d4942b
saiful-azree <azreeayie24@gmail.com>
2023-03-27 15:29:06 +0800

    Exercise Variable

---
+++ index.js
@@ -0,0 +1,22 @@
+//Exercise String and Number
+let data1 = true
+let data2 = "1.25"
+//console.log(data1, typeof data1)
+//console.log(data2, typeof data2)
+
+let data3 = data1 + data2
+//console.log(data3, typeof data3)
+
+//Exercise Array
+let data = ["one", 2, true]
+//console.log(data[2], typeof data[2])
+
+//Exercise Object
+let user = {
+    name: "sepul",
+    faculty: "FKEKK",
+    phone: {mobile: "11223344", office: "12345678", home: "24681012"},
+    email: "sepul@utem.com"
+}
+console.log(user.phone.office)
+console.log(user.phone.mobile)
